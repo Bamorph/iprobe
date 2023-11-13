09193f1dc0ea561f934f7cac078d66dd7e99556f
Bamorph <142395807+Bamorph@users.noreply.github.com>
2023-11-13 15:52:18 +1100

    Update main.go
    add flag to filter out ipv6 addresses

--- main.go
+++ main.go
@@ -2,27 +2,44 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 	"os"
 )
 
 func main() {
+	ipv6Flag := flag.Bool("ipv6", false, "Resolve IPv6 addresses")
+
+	flag.Parse()
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
 		hostname := scanner.Text()
 
-		// Resolve the hostname to an IP address
-		ipAddresses, err := net.LookupHost(hostname)
+		// Resolve the hostname to IP addresses based on the flag
+		var ipAddresses []string
+		var err error
+
+		if *ipv6Flag {
+			ipAddresses, err = net.LookupIP(hostname)
+		} else {
+			ipAddresses, err = net.LookupIP(hostname)
+		}
+
 		if err != nil {
-			// Handle errors, and consider security implications
+			// Handle errors and consider security implications
 			fmt.Printf("Error resolving %s: %v\n", hostname, err)
 			continue
 		}
 
-		// Print the IP addresses to the standard output
+		// Filter and print the IP addresses
 		for _, ip := range ipAddresses {
+			// Filter for IPv4 or IPv6 based on the flag
+			if !*ipv6Flag && ip.To4() == nil {
+				continue // Skip IPv6 if not requested
+			}
 			fmt.Println(ip)
 		}
 	}
